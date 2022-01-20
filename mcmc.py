9e978e59da0b73e026caf84406a060f5ae76ebdc
Sean Luo's Yoga <41530060+lu0x1a0@users.noreply.github.com>
2022-01-20 21:39:30 +1100

    plan to sample and fit gan with optimization functions as likelihood core and domain as random variable

---
+++ mcmc.py
@@ -0,0 +1,13 @@
+def core():
+    pass
+def prior():
+    pass
+def posterior():
+    pass
+
+class MCMC:
+    def __init__(self,totalsamples):
+        self.totalsamples = totalsamples
+    def sampler():
+        for i in range(self.totalsamples):
+            pass
