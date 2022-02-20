34c5072b73d7514abb91f8c8ad2f2cb896b1299e
Sean Luo's Yoga <41530060+lu0x1a0@users.noreply.github.com>
2022-02-20 14:23:06 +1100

    some solid implementation of rastrigin

--- mcmc.py
+++ mcmc.py
@@ -1,12 +1,13 @@
 import torch 
 from abc import ABC,abstractmethod
 class TargetDistribution(ABC):
-    @abstractmethod
-    def __init__(self,totalsamples):
-        pass
-    @abstractmethod
-    def propose(self):
-        pass
+    def __init__(self,totalsamples,dimension):
+        # samples
+        self._samples_store = torch.zeros(totalsamples,dimension)
+        self._hist_likelihoods = torch.zeros(totalsamples)
+        self._last_sample_index = -1
+        # meta statistics
+        self._accepted_count = 0
     @abstractmethod
     def proposal_dist(self,condition):
         pass
@@ -14,34 +15,41 @@ class TargetDistribution(ABC):
     def prior(self):
         pass
     @abstractmethod
-    def log_likelihood(sample):
+    def log_likelihood(self,sample):
         pass
-
-    def log_likelihood_iter(i):
+    
+    def init_theta(self):
+        pass
+    def log_likelihood_iter(self,i):
         return self._hist_likelihoods[i]
-    @abstractmethod
+    def last_sample(self):
+        return self._samples_store[self._last_sample_index]
     def accept(self,newsample):
-        pass
-    @abstractmethod
+        self._last_sample_index += 1
+        self._samples_store[self._last_sample_index] = newsample
+
+        self.accepted_count += 1
     def reject(self):
-        pass
+        self._last_sample_index += 1
+        self._samples_store[self._last_sample_index] = self._samples_store[self._last_sample_index-1]
+        
+
 class Rastrigin(TargetDistribution):
-    def __init__(self,totalsamples,dimension):
-        self._samples_store = torch.zeros(totalsamples,dimension)
-        self._hist_likelihoods = torch.zeros(totalsamples)
-    def propose(self):
-        #generate from uniform grid.
-        return torch.rand(2)*10.24-5.12
-    def propose2(self):
-        # use random walk
-        pass
-    def proposal_dist(self, condition):
-        # since its a constant, and only used for proportionality to calc ratio
-        # use 1 lazily
-        return 1
+    def __init__(self,totalsamples,dimension=2, proposal_type = "unif"):
+        super().__init__(totalsamples,dimension)
+        ... #unif or normal 
+        self.proposal_type = proposal_type
+    def proposal_dist(self, lastsample):
+        # assume all points equally likely.
+        return torch.distributions.Uniform(low = -5.14, high = 5.14)
+        # assume points from normal distriution with variance 4.
+        return torch.distributions.Normal(loc = lastsample, scale = torch.Tensor([2,2]))
     def prior(self):
-        # coincidencially identical to proposal
-        pass
+        # assume all points between -5.14, 5.14 as equally likely
+        return torch.distributions.Uniform(low = -5.14, high = 5.14)
+    def log_likelihood(self, sample):
+        y = A*n+torch.sum(torch.square(sample)-A*torch.cos(2*PI*sample),dim=0)
+        return torch.log(-y)
 class Regress1(TargetDistribution):
     pass
 class Regress1ParallelTempering(TargetDistribution):
@@ -68,12 +76,12 @@ class MetropolisHasting:
         self.totalsamples = totalsamples
         self.target = distributionclass(self.totalsamples)
         
-    def sampler():
+    def performSample(self):
         theta_last = self.target.init_theta()
         self.target.log_likelihood(theta_last)
         for i in range(1,self.totalsamples):
             # generate proposal theta* \sim q(theta|theta_last)
-            theta_prop = self.target.propose()
+            theta_prop = self.target.proposal_dist(last).sample()
 
             log_proposal_ratio = ( 
                 self.target.proposal_dist(theta_prop).log_prob(theta_last) - 
@@ -104,3 +112,8 @@ class MetropolisHasting:
             else:
                 #reject proposal
                 self.target.reject()
+
+if __name__ == "__main__":
+    mhsampler = MetropolisHasting(5000,Rastrigin)
+    mhsampler.performSample()
+    print(mhsampler.target._last_sample_index)
