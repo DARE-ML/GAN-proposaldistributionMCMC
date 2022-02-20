130a30750069130e00c76ba35f76d8e0508e8186
Sean Luo's Yoga <41530060+lu0x1a0@users.noreply.github.com>
2022-02-20 17:28:08 +1100

    rastrigin rw sampling working, need tuning and langevin

--- mcmc.py
+++ mcmc.py
@@ -1,8 +1,11 @@
-import torch 
+import torch, math
 from abc import ABC,abstractmethod
 class TargetDistribution(ABC):
     def __init__(self,totalsamples,dimension):
         # samples
+        self.dimension = dimension
+        self.totalsamples = totalsamples
+
         self._samples_store = torch.zeros(totalsamples,dimension)
         self._hist_likelihoods = torch.zeros(totalsamples)
         self._last_sample_index = -1
@@ -17,21 +20,23 @@ class TargetDistribution(ABC):
     @abstractmethod
     def log_likelihood(self,sample):
         pass
-    
+    @abstractmethod
     def init_theta(self):
         pass
+
     def log_likelihood_iter(self,i):
         return self._hist_likelihoods[i]
     def last_sample(self):
         return self._samples_store[self._last_sample_index]
-    def accept(self,newsample):
+    def accept(self,newsample,newsamplelikelihood):
         self._last_sample_index += 1
         self._samples_store[self._last_sample_index] = newsample
-
-        self.accepted_count += 1
+        self._hist_likelihoods[self._last_sample_index] = newsamplelikelihood
+        self._accepted_count += 1
     def reject(self):
         self._last_sample_index += 1
         self._samples_store[self._last_sample_index] = self._samples_store[self._last_sample_index-1]
+        self._hist_likelihoods[self._last_sample_index] = self._hist_likelihoods[self._last_sample_index-1]
         
 
 class Rastrigin(TargetDistribution):
@@ -39,17 +44,43 @@ class Rastrigin(TargetDistribution):
         super().__init__(totalsamples,dimension)
         ... #unif or normal 
         self.proposal_type = proposal_type
+    def init_theta(self):
+        first_sample = torch.Tensor([3,3])
+        first_sample_likelihood = self.log_likelihood(first_sample)
+        self.accept(first_sample,first_sample_likelihood)
+    def log_likelihood(self, sample):
+        A,PI = 10,torch.tensor(math.pi)
+        
+        #rastrigin
+        #y = A*self.dimension+torch.sum(torch.square(sample)-A*torch.cos(2*PI*sample),dim=0)
+
+        # shifted, not normalized, inverted, rastrigin 
+        y = -torch.sum(torch.square(sample)-A*torch.cos(2*PI*sample),dim=0) + 36*self.dimension
+        return torch.log(y)
+class RastriginRandomWalkUnifPrior(Rastrigin):
     def proposal_dist(self, lastsample):
-        # assume all points equally likely.
-        return torch.distributions.Uniform(low = -5.14, high = 5.14)
-        # assume points from normal distriution with variance 4.
-        return torch.distributions.Normal(loc = lastsample, scale = torch.Tensor([2,2]))
+        # assume all points equally likely. UNIFORM
+        #multunif = torch.distributions.Uniform(low = torch.Tensor([-5.14,-5.14]), high = torch.Tensor([5.14,5.14]))
+        #return multunif
+
+        # assume points from normal distriution with variance 4. RANDOM WALK
+        multnorm = torch.distributions.Normal(loc = lastsample, scale = torch.Tensor([0.1,0.1]))
+        return multnorm
     def prior(self):
         # assume all points between -5.14, 5.14 as equally likely
-        return torch.distributions.Uniform(low = -5.14, high = 5.14)
-    def log_likelihood(self, sample):
-        y = A*n+torch.sum(torch.square(sample)-A*torch.cos(2*PI*sample),dim=0)
-        return torch.log(-y)
+        multunif = torch.distributions.Uniform(low = torch.Tensor([-5.14*20,-5.14*20]), high = torch.Tensor([5.14*20,5.14*20]))
+        return multunif
+
+class RastriginRandomWalkNormPrior(Rastrigin):
+    def proposal_dist(self, lastsample):
+        # assume points from normal distriution with variance 4. RANDOM WALK
+        multnorm = torch.distributions.Normal(loc = lastsample, scale = torch.Tensor([0.1,0.1]))
+        return multnorm
+    def prior(self):        
+        # assume points from prior
+        multnorm = torch.distributions.Normal(loc = torch.Tensor([0,0]), scale = torch.Tensor([2,2]))
+        return multnorm
+
 class Regress1(TargetDistribution):
     pass
 class Regress1ParallelTempering(TargetDistribution):
@@ -77,23 +108,26 @@ class MetropolisHasting:
         self.target = distributionclass(self.totalsamples)
         
     def performSample(self):
-        theta_last = self.target.init_theta()
-        self.target.log_likelihood(theta_last)
+        # init first sample
+        self.target.init_theta()
+        
         for i in range(1,self.totalsamples):
             # generate proposal theta* \sim q(theta|theta_last)
-            theta_prop = self.target.proposal_dist(last).sample()
+            theta_last = self.target.last_sample()
+            theta_prop = self.target.proposal_dist(theta_last).sample()
 
             log_proposal_ratio = ( 
-                self.target.proposal_dist(theta_prop).log_prob(theta_last) - 
-                self.target.proposal_dist(theta_last).log_prob(theta_prop)
+                torch.sum(self.target.proposal_dist(theta_prop).log_prob(theta_last)) - 
+                torch.sum(self.target.proposal_dist(theta_last).log_prob(theta_prop))
             )
             log_prior_ratio = (
-                self.target.prior().log_prob(theta_prop) - 
-                self.target.prior().log_prob(theta_last)
+                torch.sum(self.target.prior().log_prob(theta_prop)) - 
+                torch.sum(self.target.prior().log_prob(theta_last))
             )
+            prop_likelihood = torch.sum(self.target.log_likelihood(theta_prop)) 
             log_likelihood_ratio = (
-                self.target.log_likelihood(theta_prop) - 
-                self.target.log_likelihood_iter(i-1)               
+                prop_likelihood - 
+                torch.sum(self.target.log_likelihood_iter(i-1)  )             
             )
             # u \sim U(0,1)
             u = torch.rand(1)
@@ -108,12 +142,26 @@ class MetropolisHasting:
                 alpha = 1
             if u < alpha:
                 #accept proposal
-                self.target.accept(theta_prop)
+                self.target.accept(theta_prop,prop_likelihood)
             else:
                 #reject proposal
                 self.target.reject()
 
 if __name__ == "__main__":
-    mhsampler = MetropolisHasting(5000,Rastrigin)
+    mhsampler = MetropolisHasting(10000,RastriginRandomWalk)
     mhsampler.performSample()
     print(mhsampler.target._last_sample_index)
+    
+    X = mhsampler.target._samples_store
+    import matplotlib.pyplot as plt
+    #plt.scatter(X[:, 0], X[:, 1])#, s=50, c = truth)
+    #plt.title(f"Rastrigin sampling")
+    #plt.xlabel("x")
+    #plt.ylabel("y")
+    #plt.show()
+
+    x,y = X[:,0].numpy(), X[:,1].numpy()
+    h =plt.hist2d(x, y,bins = 100)
+    plt.colorbar(h[3])
+    plt.show()
+    bp = -1
