fddc4652cfe6ba86dc10a7a0c2ff50064d9a8c7b
Sean Luo's Yoga <41530060+lu0x1a0@users.noreply.github.com>
2022-03-06 15:19:14 +1100

    code for mixture density gan

--- Code/gan.py
+++ Code/gan.py
@@ -102,17 +102,6 @@ class VanillaTrainTogether(VanillaTrain):
     """_summary_
         for each minibatch train discriminator and generator
     """
-    def __init__(self,
-        epochs:         int, 
-        goptim:         torch.optim.Optimizer,
-        doptim:         torch.optim.Optimizer,
-        generator:      torch.nn.Module, 
-        discriminator:  torch.nn.Module, 
-        dataloader:     torch.utils.data.Dataset,
-        latentdim:      tuple
-    ):
-        super().__init__(epochs,goptim,doptim,generator,discriminator,dataloader,latentdim)
-        self.viewFreq = 5 # epochs
 
     def train(self):
         for e in range(self.epochs):
@@ -157,6 +146,17 @@ class VanillaTrainTogether_MNIST(VanillaTrainTogether):
 
         have custom view during training for mnist
     """
+    def __init__(self,
+        epochs:         int, 
+        goptim:         torch.optim.Optimizer,
+        doptim:         torch.optim.Optimizer,
+        generator:      torch.nn.Module, 
+        discriminator:  torch.nn.Module, 
+        dataloader:     torch.utils.data.Dataset,
+        latentdim:      tuple
+    ):
+        super().__init__(epochs,goptim,doptim,generator,discriminator,dataloader,latentdim)
+        self.viewFreq = 5 # epochs
     def train(self):
         for e in range(self.epochs):
             for b,(minibatch,_unused_label) in enumerate(self.dataloader):
@@ -238,7 +238,42 @@ class WassensteinGPTogetherTrain_MNIST(VanillaTrainTogether_MNIST):
         gradients = gradients.view(batch_size, -1)
         grad_norm = gradients.norm(2, 1)
         return torch.mean((grad_norm - 1) ** 2)
-class MixtureSimplexTrain(VanillaTrainTogether):
+from simplex import simplex_params
+import torch.distributions as D 
+class MixtureGaus:
+    def __init__(self,disc_dimension=3,sigma_scale = 0.05):
+        # the number of modes/standard normal = number of vertices in a simplex:
+        #   the same as disc_dimension + 1, i.e. 2d = triangle(3 vertices), 3d = tetrahedron(4 vertices), 
+        # in the paper, the h-param are set as follow: 
+        # disc_dimension = 9: -> 10 gaus mode 
+        # sigma_scale = 0.25: -> individual std of 0.5
+        # distance between each mode is 1
+        self.mu, self.sigma, self.weight = simplex_params(disc_dimension)
+        # default sigma is independent and of scale 0.25, let it be smaller
+        self.gaus = D.Independent(D.MultivariateNormal(torch.tensor(self.mu),torch.tensor(self.sigma/0.25*sigma_scale)),0)
+        self.weight = D.Categorical(torch.tensor(weight))
+        self.gausMixture = D.MixtureSameFamily(self.weight,self.gaus)
+        # the max of pdf should be close to one of the modes, i.e. use that as a starter point to optimize
+        
+        _xopt , fopt, _iter, _funcalls, _warnflag = optimize.fmin(lambda x:(-torch.exp(self.gausMixture.log_prob(x)) ), 
+            self.mu[0,:,:].detach().clone(),maxiter = 100,full_output=True) 
+        self.num_tol = 1e-8 #numerical tolerance 
+        self.maxval = -fopt + self.num_tol
+        #return self.loss(x,y)
+class MixtureSimplexTrainTogether_MNIST(VanillaTrainTogether_MNIST):    
+    def __init__(self,
+        epochs:         int, 
+        goptim:         torch.optim.Optimizer,
+        doptim:         torch.optim.Optimizer,
+        generator:      torch.nn.Module, 
+        discriminator:  torch.nn.Module, 
+        dataloader:     torch.utils.data.Dataset,
+        latentdim:      tuple,
+        disc_dimension: int                         =9,
+        sigma_scale:    float                       =0.05
+    ):
+        super().__init__(epochs,goptim,doptim,generator,discriminator,dataloader,latentdim)
+        self.mixture = MixtureGaus(disc_dimension=disc_dimension,sigma_scale=sigma_scale)
     def trainDisc(self,batch):
         # sample from latent
         lat = self.latent.sample(torch.Size([batch.shape[0],*self.latentdim])).to(self.device)
@@ -247,11 +282,15 @@ class MixtureSimplexTrain(VanillaTrainTogether):
         ...
         # update disc
         self.doptim.zero_grad()
-        dloss = ( 
-            self.discriminator(fake_batch).mean() -
-            self.discriminator(batch).mean() +
-            self.compute_gp(self.discriminator,batch.to(self.device),fake_batch)
-        ) 
+        dloss = torch.log().mean()
+        # this is a target to maximize so mult by -1
+        dloss = - (
+            self.mixture.gausMixture.log_prob(self.discriminator(batch)).mean() +
+            torch.log(
+                self.mixture.maxval - 
+                torch.exp(self.mixture.gausMixture.log_prob(self.discriminator(fake_batch)))
+            ).mean()
+        )
         dloss.backward()
         self.doptim.step()
     def trainGen(self,batch):
@@ -260,6 +299,9 @@ class MixtureSimplexTrain(VanillaTrainTogether):
         fake_batch = self.generator(lat)
         # update gen
         self.goptim.zero_grad()
-        gloss = -self.discriminator(fake_batch).mean()
+        gloss = torch.log(
+            self.mixture.maxval - 
+            torch.exp(self.mixture.gausMixture.log_prob(self.discriminator(fake_batch).mean()))
+        ).mean()
         gloss.backward()
         self.goptim.step()
