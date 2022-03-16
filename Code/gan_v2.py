f4f944dd710983da895e405f8080a29364efacd9
Sean Luo's Yoga <41530060+lu0x1a0@users.noreply.github.com>
2022-03-16 13:20:15 +1100

    finished prelim refactor

--- Code/gan_v2.py
+++ Code/gan_v2.py
@@ -2,50 +2,254 @@ from typing import Callable
 from abc import ABC, abstractmethod
 from torch import nn, optim, distributions
 
-class GAN:
+class NetWrapper(nn.module):
+    def __init__(self,base,out_act):
+        self.base = base
+        self.out_act = out_act
+    def forward(self,x):
+        return self.out_act(self.base(x))
+
+class vanilla_disc_act(nn.Module):
+    def __init__(self,input_dim):
+        self.act = nn.Sequential(
+            nn.Linear(input_dim,1),
+            nn.Sigmoid()
+        )
+    def forward(self,x):
+        return self.act(x)
+class wgan_disc_act(nn.Module):
+    def __init__(self,input_dim):
+        self.act = nn.Linear(input_dim, 1)
+    def forward(self,x):
+        return self.act(x)
+class mdgan_disc_act(nn.Module):
+    def __init__(self,input_dim,out_dim):
+        self.act = nn.Linear(input_dim,out_dim)
+    def forward(self,x):
+        return self.act(x)
+
+class GANLoop(ABC):
     def __init__(
         self,
-        gen: nn.Module,
-        dis: nn.Module,
-        genoptim: optim.Optimizer,
-        disoptim: optim.Optimizer,
         genStep: Callable[[nn.Module,optim.Optimizer],float], # input: net,optim, output loss, includes loss declaration, backwards, steps and zero grad
         disStep: Callable[[nn.Module,optim.Optimizer],float],
-        trainloop: Callable[[genStep,disStep],None]
+        goptim: optim.Optimizer,
+        doptim: optim.Optimizer, 
+        latent: Callable,
+        gen: nn.Module,
+        dis: nn.Module,
+        dataloader: torch.utils.data.Dataloader,
+        epochs: int,
+        device: torch.device
     ):
-        ...
+        self.genStep = genStep 
+        self.disStep = disStep 
+        self.goptim = goptim 
+        self.doptim = doptim 
+        self.latent = latent 
+        self.gen = gen 
+        self.dis = dis 
+        self.dataloader = dataloader 
+        self.epochs = epochs 
+        self.device = device
+    @abstractmethod
     def train(self):
-        meta = self.trainingLoop(self.genStep,self.disStep)
-        return meta
+        ...
 
-class Loop:
-    def __init__():
-        pass
-    def startloop(self,k):
-        pass
-def togetherLoop():
-    ...
+class TogetherLoop(GANLoop):
+    def train(self):
+        for e in range(self.epochs):
+            for i,batch in enumerate(self.dataloader):
+                # train discriminator
+                self.disStep(
+                    self.gen,self.dis,self.doptim,self.latent,
+                    batch.to(self.device),self.device
+                )
+                # train generator
+                self.genStep(
+                    self.gen,self.dis,self.goptim,self.latent,
+                    batch.to(self.device),self.device
+                )
+class UnwantedLabelLoop(GANLoop):
+    def train(self):
+        for e in range(self.epochs):
+            for i,(batch,_useless_label) in enumerate(self.dataloader):
+                # train discriminator
+                self.disStep(
+                    self.gen,self.dis,self.doptim,self.latent,
+                    batch.to(self.device),self.device
+                )
+                # train generator
+                self.genStep(
+                    self.gen,self.dis,self.goptim,self.latent,
+                    batch.to(self.device),self.device
+                )
 def OneGenPerEpochLoop():
     ...
 def KGenPerEpochLoop(k):
     ...
 
-class TrainNetworkStep:
-    def __init__(self,net,optimizer):
-        pass
-    def step(self):
-        pass
-def VanillaDiscriminatorStep():
-    ...
-def VanillaGeneratorStep():
-    ...
+# currently assumes all latent of generator are normal
 
-def WassersteinCriticStep():
+#%% vanilla step
+def VanillaDiscriminatorStep(generator,discriminator,optimizer,latent,batch,device):
+    fake_batch = generator(latent(batch.shape[0]).to(device))
+    # update disc
+    y = torch.cat( (torch.ones([batch.shape[0],1],device = device),torch.zeros([batch.shape[0],1],device = device)) )
+    y.to(device)
+    x = discriminator(torch.cat((batch.to(device),fake_batch)))
+    optimizer.zero_grad()
+    dloss = torch.nn.BCELoss()(x,y)
+    dloss.backward()
+    optimizer.step()
+    return dloss
+def VanillaGeneratorStep(generator,discriminator,optimizer,latent,batch,device):
+    # sample from latent
+    fake_batch = generator(latent(batch.shape[0]).to(device))    
+    # update gen
+    y = torch.zeros([batch.shape[0],1],device = device)
+    x = discriminator(fake_batch)
+    optimizer.zero_grad()
+    gloss = -torch.nn.BCELoss()(x,y)
+    gloss.backward()
+    optimizer.step()
+    return gloss
+#%% wgan-gp
+def WassersteinCriticStep(generator,discriminator,optimizer,latent,batch,device):
+    # sample from latent
+    fake_batch = generator(latent(batch.shape[0]).to(device))    
+    # sample from data
     ...
-def WassersteinGeneratorStep():
-    ...
+    # update disc
+    optimizer.zero_grad()
 
-def MixtureDensityGeneratorStep():
-    ...
-def MixtureDensityCriticStep():
-    ...
+    dloss = ( 
+        discriminator(fake_batch).mean() -
+        discriminator(batch).mean() +
+        __compute_gp(discriminator,batch,fake_batch)
+    ) 
+    dloss.backward()
+    optimizer.step()
+    return dloss
+def WassersteinGeneratorStep(generator,discriminator,optimizer,latent,batch,device):
+    # sample from latent
+    fake_batch = generator(latent(batch.shape[0]).to(device)) 
+    # update gen
+    optimizer.zero_grad()
+    gloss = -self.discriminator(fake_batch).mean()
+    gloss.backward()
+    optimizer.step()
+    return gloss
+def __compute_gp(discriminator, real_data, fake_data):
+    batch_size = real_data.size(0)
+    # Sample Epsilon from uniform distribution
+    eps = torch.rand(batch_size, *[1]*len(real_data.shape[1:])).to(real_data.device)
+    eps = eps.expand_as(real_data)
+    
+    # Interpolation between real data and fake data.
+    interpolation = eps * real_data + (1 - eps) * fake_data
+    
+    # get logits for interpolated images
+    interp_logits = discriminator(interpolation)
+    grad_outputs = torch.ones_like(interp_logits)
+    
+    # Compute Gradients
+    gradients = autograd.grad(
+        outputs=interp_logits,
+        inputs=interpolation,
+        grad_outputs=grad_outputs,
+        create_graph=True,
+        retain_graph=True,
+    )[0]
+    
+    # Compute and return Gradient Norm
+    gradients = gradients.view(batch_size, -1)
+    grad_norm = gradients.norm(2, 1)
+    return torch.mean((grad_norm - 1) ** 2)
+#%% mdgan
+class MixtureDensityCriticStep:
+    def __init__(
+        self,
+        mixture = MixtureGaus(disc_dimension=4,sigma_scale=0.25)
+    ):
+        self.mixture = mixture
+    def __call__(self,generator,discriminator,optimizer,latent,batch,device):
+        # sample from latent
+        fake_batch = generator(latent(batch.shape[0]).to(device))    
+        # sample from data
+        ...
+        # update disc
+        optimizer.zero_grad()
+        # this is a target to maximize so mult by -1
+        dloss = - (
+            self.mixture.gausMixture.log_prob(discriminator(batch)).mean() +
+            torch.log(
+                self.mixture.maxval - 
+                #1-
+                torch.exp(self.mixture.gausMixture.log_prob(discriminator(fake_batch)))
+            ).mean()
+        )
+        dloss.backward()
+        optimizer.step()
+        return dloss
+class MixtureDensityGeneratorStep:
+    def __init__(
+        self,
+        mixture = MixtureGaus(disc_dimension=4,sigma_scale=0.25)
+    ):
+        self.mixture = mixture
+    def __call__(self,generator,discriminator,optimizer,latent,batch,device):
+        # sample from latent
+        fake_batch = generator(latent(batch.shape[0]).to(device))    
+        # update gen
+        optimizer.zero_grad()
+        gloss = torch.log(
+            self.mixture.maxval - 
+            #1-
+            torch.exp(self.mixture.gausMixture.log_prob(discriminator(fake_batch)))
+        ).mean()
+        gloss.backward()
+        optimizer.step()
+        return gloss
+from .simplex import simplex_params
+from scipy import optimize
+class MixtureGaus:
+    def __init__(self,disc_dimension=9,sigma_scale = 0.25):
+        # the number of modes/standard normal = number of vertices in a simplex:
+        #   the same as disc_dimension + 1, i.e. 2d = triangle(3 vertices), 3d = tetrahedron(4 vertices), 
+        # in the paper, the h-param are set as follow: 
+        # disc_dimension = 9: -> 10 gaus mode 
+        # sigma_scale = 0.25: -> individual std of 0.5
+        # distance between each mode is 1
+        self.device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+        
+        self.sigma_scale = sigma_scale
+        self.mu, self.sigma, self.weight = simplex_params(disc_dimension)
+        # default sigma is independent and of scale 0.25, let it be smaller
+        self.gaus = distributions.Independent(distributions.MultivariateNormal(
+            torch.tensor(self.mu).to(self.device),
+            torch.tensor(self.sigma/0.25*sigma_scale).to(self.device)
+        ),0)
+        self.c_weight = distributions.Categorical(torch.tensor(self.weight).to(self.device))
+        self.gausMixture = distributions.MixtureSameFamily(self.c_weight,self.gaus)
+
+        self.findMax()
+
+    def findMax(self):
+        # the max of pdf should be close to one of the modes, i.e. use that as a starter point to optimize
+        #maxiter = 2000 works for up to (9 dim 10 mode)     
+        gaus_cpu = distributions.Independent(distributions.MultivariateNormal(
+            torch.tensor(self.mu),
+            torch.tensor(self.sigma/0.25*self.sigma_scale)
+        ),0)
+        c_weight_cpu = distributions.Categorical(torch.tensor(self.weight))
+        gausMixture_cpu = distributions.MixtureSameFamily(c_weight_cpu,gaus_cpu)
+
+
+        _xopt , fopt, _iter, _funcalls, _warnflag = optimize.fmin(lambda x:(-gausMixture_cpu.log_prob(torch.FloatTensor(x)) ), 
+            torch.FloatTensor(self.mu[0,:]),maxiter = 2000,full_output=True) 
+        # numerical tolerance
+        self.num_tol = 1e-8  
+        # maxval to be used for in loss function, it is the true prob, not log_prob
+        self._lg_maxval = self.gausMixture.log_prob(torch.FloatTensor(_xopt).to(self.device))
+        self.maxval = torch.exp(self.gausMixture.log_prob(torch.FloatTensor(_xopt).to(self.device))) + self.num_tol
--- Code/mcmc_v2.py
+++ Code/mcmc_v2.py
@@ -1,31 +1,305 @@
 # reimplements metropolis hasting wrt the factory method
 # goal is to combines mhgan and normal mh code 
 import torch, math
+import torch.distributions as D
 from abc import ABC,abstractmethod
 from typing import Callable
+
+class AcceptRejectBlueprint(ABC):
+    @abstractmethod
+    def __call__(self,propose,last)->int:
+        """_summary_
+
+        Returns:
+            int: count of accepted samples
+        """
+        pass
+    
+class ProposeFuncBlueprint(ABC):
+    @abstractmethod
+    def propose(self,last_sample):
+        """Propose the next sample base or without the last, 
+            e.g. random walk with last sample as mean, langevin gradient
+        """
+        pass
+    @abstractmethod
+    def log_prob(self,sample,cond_sample):
+        pass
+class TargetFuncBlueprint(ABC):
+    @abstractmethod
+    def eval_log(self,sample):
+        pass
+
+class MH_GAN_and_RW:
+    def __init__(self, normalMH, ganloop, ganMH, cycles):
+        self.normalMH = normalMH
+        self.trainGAN = ganloop
+        self.ganMH    = ganMH
+        self.cycles   = cycles
+    def startSampling(self):
+        for cycle in range(cycles):
+            samples = self.normalMH.startSampling()
+            self.trainGAN()
+            gensamples = self.ganMH.startSampling()
+            # 1. rank gensamples by discriminator score
+            # 2. select 3 subsets of gensamples: high, med, low rank by discriminator
+            # 3. evaluate all 3 sets for real 
+            # 4. select batch of good ones and use as initial samples of normalMH from next cycle
 class MetropolisHastings:
+    """ A Generic Factory for Metropolis Hasting with different:
+            1. accept reject criteria
+            2. number of chains per iteration
+        Used together with:
+            1. AcceptReject Callable
+            2. ProposeFunc  Callable
+            3. TargetFuc    Callable
+        Hierarchy goes as this: 
+            MH -> 
+                propose, 
+                accept_reject ->
+                    propose_prob,
+                    target_intensity
+
+    """
+
     def __init__(self,
-        target_func: Callable[torch.Tensor,torch.FloatTensor], # evaluates the intensity/(scaled probability) of a sample
-                     #DensityFunction Class?
-        accept_prob_func: Callable[...,torch.FloatTensor], # accept or reject new samples
-        n_chains: int = 1, # how many chains to run in parallel
+        #target_func:        Callable[torch.Tensor,torch.FloatTensor], # evaluates the intensity/(scaled probability) of a sample
+        propose_func:       ProposeFuncBlueprint, # input: last sample, output: new sample [n_sample, *dim of single sample]
+        accept_reject:      AcceptRejectBlueprint, # accept or reject new samples
+
+        initial_sample:     torch.Tensor,   # n initial samples, n = n_chains
+        device:             torch.device,
+
+        iterations:         int = 1000,     # How many samples per chain
+        n_chains:           int = 1,        # META FROM PROPOSE_FUNC: how many chains to run in parallel,       
     ):
-        self.target_func = target_func
-        self.accept_prob_func = accept_prob_func
-        self.sample_store = torch.zeroes()
-        ...
-    def SequentialChainSampling(
-        self
-    ):
-        self.sample_store[0,:] = ... # initial sample
+        self.accept_reject = accept_reject
+        self.propose_func = propose_func
+        self.device = device
+        self.iterations = iterations
+        self.n_chains = n_chains
+        self.sample_store = torch.zeros(iterations,*initial_sample.shape,device = device)
+        self.sample_store[0,:] = initial_sample
+    def startSampling(self):
+        #print("store:",self.sample_store,self.sample_store.shape)
+        last_sample = self.sample_store[0,:]
+        #print("last sample:",last_sample,last_sample.shape)
         
-        for i in range(1,self.iters):
-            ...
-    def ParallelChainSampling(
-        self
+        accept_no = 0
+        total_no  = 0
+        for i in range(1,self.iterations):
+            new_sample = self.propose_func.propose(last_sample)
+            #print("NEW SAMPLE:",new_sample)
+            # NOTE: if return batch of entire last sample is 
+            #       considered as accepting the past = rejecting propose.
+            #       Hence, accepted 
+            accept_count,accepted = self.accept_reject(new_sample,last_sample)
+            #print("Accepted?:",accept)
+            if accept_count:    
+                last_sample = accepted
+            self.sample_store[i,:] = accepted
+            accept_no += accept_count
+            total_no  += new_sample.shape[0]
+        return self.sample_store, accept_no, total_no
+#%% random walk for rastrigin
+class MHAcceptReject(AcceptRejectBlueprint):
+    """
+        designed only to work with 1 chain
+    """
+    def __init__(
+        self,
+        propose_func: ProposeFuncBlueprint,
+        target_func: TargetFuncBlueprint,
+        device:     torch.device
     ):
+        self.last    = None #  assumes of the dimension: [n_samples, *shape_of_a_sample]
+        self.last_eval_log = -1 # 0  exp(-1)<1, i.e. first sample's probability is less than 1
+        self.propose_func = propose_func
+        self.target_func = target_func
+        self.device = device
+    def __call__(self,propose,last):
+        #accept reject
+        # min(1, pi(w*|x)/pi(wi|x) *  q(wi|w*)/q(w*|wi) )    
+        # min(1, pi(x|w*)/pi(x|wi) * p(w*)/p(wi) *  q(wi|w*)/q(w*|wi) )    
+        u = torch.rand(propose.shape[0],device = self.device)
+
+        p_xp = self.target_func.eval_log(propose)  
+        #print("proposal eval:",p_xp)
+        p_xi = self.last_eval_log 
+        q_xi_xp = self.propose_func.log_prob(last,propose) 
+        q_xp_xi = self.propose_func.log_prob(propose,last)
+        pre_alpha = torch.exp(p_xp-p_xi+q_xi_xp-q_xp_xi)
+        #print("ln p:",p_xp,p_xi,q_xi_xp,q_xp_xi)
+        #print("u,a:",u,pre_alpha)
+        if u<min(1,pre_alpha):
+            self.last = propose
+            self.last_eval_log = p_xp
+            return 1, propose
+        else:
+            return 0, last
+class RandomWalkProposal(ProposeFuncBlueprint):
+    def __init__(self,std,device,n_samples = 1):
+        self.device = device
+        self.std = std
+        self.n_samples = n_samples
+    def propose(self,last_sample):
+        return torch.distributions.Normal(
+            loc = last_sample,
+            scale = self.std
+        ).sample() # not .sample(n_samples) because it takes the shape of last_sample
+    def log_prob(self,sample,cond_sample):
+        # since its a constant, and only used for proportionality to calc ratio
+        # use 1 lazily
+        return 1
+
+
+from scipy.stats import truncnorm
+class TruncNorm:
+    def __init__(self, mean = 0, std = 1, low_bound = -float('inf'), up_bound= float('inf')):
+        self.mylow = low_bound
+        self.myup  = up_bound
+        self.mean = mean
+        self.std = std
+        self.low, self.high = (self.mylow - mean) / std, (self.myup - mean) / std
+    def sample(self):
+        return torch.Tensor(
+            truncnorm.rvs(
+                a=self.low,
+                b=self.high,
+                loc=self.mean,
+                scale = self.std,
+                size = (self.mean.shape) 
+            )
+        )
+    def log_prob(self,sample):
+        return torch.Tensor(truncnorm.logpdf(sample,a=self.low,b=self.high,loc=self.mean,scale = self.std))
+class TruncNormProposal(ProposeFuncBlueprint):
+    def __init__(self,std,device,dimension,low_bound = -5.14, up_bound= 5.14):
+        self.device = device
+        self.std = std
+        self.dimension = dimension
+        self.low_bound, self.up_bound = low_bound, up_bound
+    def propose(self,last_sample):
+        return TruncNorm(
+            mean = last_sample,
+            std = torch.ones(self.dimension)*self.std, 
+            low_bound= self.low_bound, up_bound = self.up_bound
+        ).sample().to(self.device) # not .sample(n_samples) because it takes the shape of last_sample
+    def log_prob(self,sample,cond_sample):
+        return torch.sum(TruncNorm(
+            mean = cond_sample,
+            std = torch.ones(self.dimension)*self.std, 
+            low_bound= self.low_bound, up_bound = self.up_bound
+        ).log_prob(sample),dim=-1).to(self.device)
+
+class RastriginTarget(TargetFuncBlueprint):
+    def __init__(self,dimension=2):
+        self.dimension = dimension
+    def eval_log(self,sample):
+        A,PI = 10,torch.tensor(math.pi)
+        
+        #rastrigin
+        #y = A*self.dimension+torch.sum(torch.square(sample)-A*torch.cos(2*PI*sample),dim=0)
+        if torch.any(torch.abs(sample)>=5.14):
+            return torch.Tensor([-float("inf")])
+        # shifted, not normalized, inverted, rastrigin 
+        y = -torch.sum(torch.square(sample)-A*torch.cos(2*PI*sample),dim=-1) + 36*self.dimension
+        #print("EVAL LOG:")
+        #print("sample:",sample,sample.shape)
+        #print("y:",y,y.shape)
+        #print("calc:",torch.square(sample)-A*torch.cos(2*PI*sample),(torch.square(sample)-A*torch.cos(2*PI*sample)).shape)
+        #print("END EVAL LOG")
+        return 3*torch.log(y)
+class GausMix2(TargetFuncBlueprint):
+    def __init__(self,dimension=2):
+        self.dimension = dimension
+        mu = torch.Tensor(
+            [[2,2],
+            [-2,-2]]
+        )
+        sigma = torch.Tensor(
+            [[[2, 0.  ],
+              [0.  , 2]],
+            [[2, 0.  ],
+              [0.  , 2]]]
+        )
+
+        gaus = D.Independent(D.MultivariateNormal(mu,sigma),0)
+        c_weight = D.Categorical(torch.tensor(torch.ones(2)))
+        self.target = gausMixture = D.MixtureSameFamily(c_weight,gaus)
+    def eval_log(self,sample):
+        return self.target.log_prob(sample)
+class Gaus2DShift(TargetFuncBlueprint):
+    def __init__(self):
         ...
-    def CombinedChainSampling(
-        self
-    ):
-        ...
+    def eval_log(self,sample):
+        return D.Normal(torch.Tensor([2,2]),torch.Tensor([1])).log_prob(sample)
+
+#%% mhgan
+class MHGANAcceptReject(AcceptRejectBlueprint):
+    def __init__(self,discriminator,n_chains,device,calibrator):
+        self.D = discriminator
+        self.batch_size = self.n_chains = n_chains
+        self.device = device
+        self.last_batch = None
+        self.last_prob = torch.zeros(self.batch_size)
+        self.calibrator = calibrator
+    def __call__(self,propose,last):
+        """
+        Args:
+            propose (_type_): _description_
+            last (_type_): _description_
+
+        Returns:
+            int: count of accepted
+        """
+        #accept reject  
+        u = torch.rand(propose.shape[0],device = self.device)
+
+        propose_prob = self.D(propose) if not self.calibrator else self.calibrator(self.D(propose))
+        alpha = torch.min(
+            torch.ones(len(propose_prob),device=self.device),
+            (1.0/self.last_prob - 1)/(1.0/propose_prob - 1)  
+        )
+
+        self.last_batch = torch.where(
+            #torch.cat([u.view(-1, 1), u.view(-1, 1)], dim=1) <= torch.cat([a.view(-1, 1), a.view(-1, 1)], dim=1),
+            u.view(-1, 1) <= a.view(-1, 1),
+            x_prop, x_last
+        )
+        #all_samples[iter,:] = x_last
+        #print("LAST_PROB_unchange",last_prob.shape)
+        #print(u.shape,a.shape)
+        #print(u.view(-1,1).shape,a.view(-1, 1).shape)
+        self.last_prob = torch.where(
+            u <= a,
+            prop_prob,last_prob  
+        )
+        return torch.sum(u<=a).item(), self.last_batch
+
+def test_rastTruncNorm():
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    proposal_rw = TruncNormProposal(std=0.5,device=device,dimension=2)
+    rast_target = RastriginTarget(dimension = 2)
+    initial_sample = torch.Tensor([[-4,-4]],device = device)
+
+    mhar = MHAcceptReject(proposal_rw,rast_target, device)
+
+    mh = MetropolisHastings(proposal_rw,mhar, initial_sample, device, iterations=1000)
+
+    samples,accepted,total = mh.startSampling()
+def test_GausMix():
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    proposal_rw = RandomWalkProposal(2, device)
+    gaus_target = GausMix2(dimension = 2)
+    initial_sample = torch.Tensor([[0,0]],device = device)
+
+    mhar = MHAcceptReject(proposal_rw,gaus_target, device)
+
+    mh = MetropolisHastings(proposal_rw,mhar, initial_sample, device, iterations=10000)
+
+    samples,accepted,total = mh.startSampling()
+if __name__ == "__main__":
+    #test_GausMix()
+    test_rastTruncNorm()
+# %%
